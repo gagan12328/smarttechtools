0c09eefa6a2088e0d68208514191a14379f252b9
Ravi Dhiman <ravi.dhiman@razorpay.com>
2021-12-09 23:10:01 +0530

    fe setup

---
+++ frontend/.prettierrc.js
@@ -0,0 +1,10 @@
+module.exports = {
+  trailingComma: 'es5',
+  tabWidth: 2,
+  semi: false,
+  singleQuote: true,
+  svelteSortOrder: 'options-styles-scripts-markup',
+  svelteStrictMode: true,
+  svelteAllowShorthand: true,
+  svelteIndentScriptAndStyle: true,
+}
---
+++ frontend/src/pages/home/index.ts
@@ -0,0 +1,7 @@
+import Page from './index.svelte'
+
+const App = new Page({
+  target: document.body,
+});
+
+export default App
