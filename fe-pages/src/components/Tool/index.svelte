9ad9058505e938f618836f989d885f11828d3d8d
Ravi Dhiman <ravid7000@gmail.com>
2022-01-10 00:05:12 +0530

    changes done

---
+++ fe-pages/src/components/Tool/index.svelte
@@ -0,0 +1,30 @@
+<script>
+  export let name;
+</script>
+
+<div class="tool">
+  <div class="tool-icon"></div>
+  <div class="tool-name">
+    {name}
+  </div>
+</div>
+
+<style>
+  .tool {
+    display: inline-block;
+  }
+
+  .tool-icon {
+    width: 60px;
+    height: 60px;
+    background-color: var(--white);
+    border-radius: 8px;
+    box-shadow: 0px 6px 9px rgba(0, 0, 0, 0.11);
+  }
+
+  .tool-name {
+    text-align: center;
+    padding-top: 0.5rem;
+    font-size: 0.8rem;
+  }
+</style>
---
+++ fe-pages/src/routes/tools.svelte
@@ -0,0 +1,32 @@
+<script>
+  import Tool from '../components/Tool/index.svelte';
+</script>
+
+<section>
+  <div class="container">
+    <h1 class="section-title text-center">All Smart Tech Tools</h1>
+    <p class="section-sub-title text-center">
+      We have built a collection of smart tools to process your documents seamlessly.
+    </p>
+
+    <div id="toolList">
+      <Tool name="PDF" />
+    </div>
+  </div>
+</section>
+
+<style>
+  .section-title {
+    font-size: 3.5rem;
+    font-weight: 700;
+  }
+
+  .section-sub-title {
+    font-size: 1.2rem;
+    font-weight: 400;
+  }
+
+  section {
+    padding: 40px 0;
+  }
+</style>
